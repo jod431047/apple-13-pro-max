e8e58d2e0a3fcc9454d88a493ff658a868d85abb
jod431047 <jod431047@gmail.com>
2023-02-28 19:31:11 +0100

    apple

---
+++ code.js
@@ -0,0 +1,8 @@
+let imgr = document.querySelector('.imgr');
+let body = document.querySelector('body');
+function phones(phone){
+    imgr.src = phone;
+}
+function colors(color){
+   body.style.background=color; 
+}
